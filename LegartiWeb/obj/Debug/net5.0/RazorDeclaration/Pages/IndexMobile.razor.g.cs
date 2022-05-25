a83dd996416df93a5dd878f770546d53e29fe6da
Ambra Zecchin <ambra.zecchin@intellitronika.com>
2022-05-25 15:29:34 +0200

    Struttura lato client della pag. Bibliocabine pt.1

--- LegartiWeb/obj/Debug/net5.0/RazorDeclaration/Pages/IndexMobile.razor.g.cs
+++ LegartiWeb/obj/Debug/net5.0/RazorDeclaration/Pages/IndexMobile.razor.g.cs
@@ -13,84 +13,84 @@ namespace LegartiWeb.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 1 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 2 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 3 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 4 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 5 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 6 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 7 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 8 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 9 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using LegartiWeb;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 10 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using LegartiWeb.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\_Imports.razor"
+#line 11 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\1_MONICA\ProgettiMiei\Legarti\Legarti\Progetto\LegartiWeb\Pages\IndexMobile.razor"
+#line 3 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\Pages\IndexMobile.razor"
 using LegArtiModel;
 
 #line default
