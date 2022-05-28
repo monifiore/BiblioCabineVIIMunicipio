7264d7df7bc3d1cdaed6441ff741e1be18b92061
Ambra Zecchin <ambra.zecchin@intellitronika.com>
2022-05-28 13:03:51 +0200

    upgrade

--- LegartiWeb/obj/Debug/net5.0/RazorDeclaration/Pages/Amministrazione/Bibliocabine/Dettaglio.razor.g.cs
+++ LegartiWeb/obj/Debug/net5.0/RazorDeclaration/Pages/Amministrazione/Bibliocabine/Dettaglio.razor.g.cs
@@ -13,91 +13,91 @@ namespace LegartiWeb.Pages.Amministrazione.Bibliocabine
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 1 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 2 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 3 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 4 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 5 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 6 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 7 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 8 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 9 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using LegartiWeb;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 10 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using LegartiWeb.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\_Imports.razor"
+#line 11 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\Pages\Amministrazione\Bibliocabine\Dettaglio.razor"
+#line 5 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\Pages\Amministrazione\Bibliocabine\Dettaglio.razor"
 using LegArtiModel;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\1_MONICA\ProjectGitHub\BiblioCabineVIIMunicipio\LegartiWeb\Pages\Amministrazione\Bibliocabine\Dettaglio.razor"
+#line 6 "C:\Users\ambra.zecchin\Desktop\Legarti\LegartiWeb\Pages\Amministrazione\Bibliocabine\Dettaglio.razor"
 using System.Globalization;
 
 #line default
